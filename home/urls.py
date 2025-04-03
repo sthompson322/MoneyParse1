b5524b9ab3f9e13a3f57f15097455ec2255b5bbf
Esha <esha.pansara23@icloud.com>
2025-04-02 22:31:09 -0400

    Created home page and about page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('reports', views.reports, name='home.reports'),
+    path('budget', views.budget, name='home.budget'),
+    path('profile', views.profile, name='home.profile'),
 ]
